3ef3c59fcffdfd8017d821951b7b07884c4d5682
Andrés Moschini <andresmoschini@gmail.com>
2022-06-08 15:35:12 -0300

    refactor: improve type definition

--- src/asset-services.ts
+++ src/asset-services.ts
@@ -1,49 +1,56 @@
-export function AssetServices() {
-  function ensureAbsoluteURLs(baseURL: string, entrypoints: string[]) {
-    const regExpIsAbsoluteURL = new RegExp("^(?:[a-z]+:)?//", "i");
-    return entrypoints.map(function (entrypoint) {
-      if (!regExpIsAbsoluteURL.test(entrypoint)) {
-        return baseURL + entrypoint;
-      }
-      return entrypoint;
+function ensureAbsoluteURLs(baseURL: string, entrypoints: string[]) {
+  const regExpIsAbsoluteURL = new RegExp("^(?:[a-z]+:)?//", "i");
+  return entrypoints.map(function (entrypoint) {
+    if (!regExpIsAbsoluteURL.test(entrypoint)) {
+      return baseURL + entrypoint;
+    }
+    return entrypoint;
+  });
+}
+
+function addRef(entrypoint: string) {
+  const pattern = /\.([0-9a-z]+)(?=[?#])|(\.)(?:[\w]+)$/gim;
+  // eslint-disable-next-line default-case
+  switch (entrypoint.match(pattern)![0]) {
+    case ".css":
+      let link = document.createElement("link");
+      link.rel = "stylesheet";
+      link.href = entrypoint;
+      (link as any).async = false;
+      document.head.appendChild(link);
+      break;
+    case ".js":
+      let script = document.createElement("script");
+      script.src = entrypoint;
+      script.async = false;
+      document.body.appendChild(script);
+      break;
+  }
+}
+
+async function load(
+  manifestURL: string,
+  sources: string[] = []
+): Promise<void> {
+  try {
+    const response = await fetch(manifestURL);
+    const data = await response.json();
+    const entrypoints = ensureAbsoluteURLs(
+      manifestURL.substring(0, manifestURL.lastIndexOf("/") + 1),
+      data.entrypoints
+    );
+    entrypoints.concat(sources).forEach((entrypoint) => {
+      addRef(entrypoint);
     });
+  } catch (error) {
+    throw new Error("Error getting assets file: " + manifestURL);
   }
+}
 
-  function addRef(entrypoint: string) {
-    const pattern = /\.([0-9a-z]+)(?=[?#])|(\.)(?:[\w]+)$/gim;
-    // eslint-disable-next-line default-case
-    switch (entrypoint.match(pattern)![0]) {
-      case ".css":
-        let link = document.createElement("link");
-        link.rel = "stylesheet";
-        link.href = entrypoint;
-        (link as any).async = false;
-        document.head.appendChild(link);
-        break;
-      case ".js":
-        let script = document.createElement("script");
-        script.src = entrypoint;
-        script.async = false;
-        document.body.appendChild(script);
-        break;
-    }
-  }
+interface IAssetServices {
+  load(manifestURL: string, sources?: string[]): Promise<void>;
+}
 
-  async function load(manifestURL: string, sources: string[] = []) {
-    try {
-      const response = await fetch(manifestURL);
-      const data = await response.json();
-      const entrypoints = ensureAbsoluteURLs(
-        manifestURL.substring(0, manifestURL.lastIndexOf("/") + 1),
-        data.entrypoints
-      );
-      entrypoints.concat(sources).forEach((entrypoint) => {
-        addRef(entrypoint);
-      });
-    } catch (error) {
-      throw new Error("Error getting assets file: " + manifestURL);
-    }
-  }
-
-  return { load: load };
+export class AssetServices implements IAssetServices {
+  load = load;
 }
