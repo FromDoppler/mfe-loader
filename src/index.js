af53c77c63e045415e468077c35c83f650e22b02
Andrés Moschini <andresmoschini@gmail.com>
2022-06-08 14:17:47 -0300

    feat: add original code

---
+++ src/index.js
@@ -0,0 +1,49 @@
+/* eslint-disable */
+(AssetServices = function () {
+  this.ensureAbsoluteURLs = (baseURL, entrypoints) => {
+    const regExpIsAbsoluteURL = new RegExp("^(?:[a-z]+:)?//", "i");
+    return entrypoints.map(function (entrypoint) {
+      if (!regExpIsAbsoluteURL.test(entrypoint)) {
+        return baseURL + entrypoint;
+      }
+      return entrypoint;
+    });
+  };
+  this.addRef = (entrypoint) => {
+    const pattern = /\.([0-9a-z]+)(?=[?#])|(\.)(?:[\w]+)$/gim;
+    switch (entrypoint.match(pattern)[0]) {
+      case ".css":
+        let link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href = entrypoint;
+        link.async = false;
+        document.head.appendChild(link);
+        break;
+      case ".js":
+        let script = document.createElement("script");
+        script.src = entrypoint;
+        script.async = false;
+        document.body.appendChild(script);
+        break;
+    }
+  };
+  this.load = (manifestURL, sources = []) => {
+    fetch(manifestURL)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        const entrypoints = ensureAbsoluteURLs(
+          manifestURL.substring(0, manifestURL.lastIndexOf("/") + 1),
+          data.entrypoints
+        );
+        entrypoints.concat(sources).forEach((entrypoint) => {
+          this.addRef(entrypoint);
+        });
+      })
+      .catch(function () {
+        throw new Error("Error getting assets file: " + manifestURL);
+      });
+  };
+  return { load: load };
+})();
