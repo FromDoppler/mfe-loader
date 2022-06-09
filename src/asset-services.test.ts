80ca7dbbe05543c9a6b6cd559f1027576db2a0e4
Andrés Moschini <andresmoschini@gmail.com>
2022-06-09 14:13:33 -0300

    test: refactor to remove duplicated test code

--- src/asset-services.test.ts
+++ src/asset-services.test.ts
@@ -28,7 +28,22 @@ describe(AssetServices.name, () => {
     });
   });
 
-  describe("load (obsolete)", () => {
+  describe.each([
+    {
+      description: "deprecated",
+      act: (
+        instance: AssetServices,
+        { manifestURL, sources }: { manifestURL: string; sources?: string[] }
+      ) => instance.load(manifestURL, sources),
+    },
+    {
+      description: "with object literal parameter",
+      act: (
+        instance: AssetServices,
+        parameters: { manifestURL: string; sources?: string[] }
+      ) => instance.load(parameters),
+    },
+  ])("load ($description)", ({ act }) => {
     it("should load manifest from specified URL", async () => {
       // Arrange
       const manifestURL =
@@ -40,7 +55,7 @@ describe(AssetServices.name, () => {
       const instance = new AssetServices(windowMoq as any);
 
       // Act
-      await instance.load(manifestURL);
+      await act(instance, { manifestURL });
 
       // Assert
       expect(windowMoq.fetch).toHaveBeenCalledTimes(1);
@@ -62,7 +77,7 @@ describe(AssetServices.name, () => {
       const instance = new AssetServices(windowMoq as any);
 
       // Act
-      await instance.load(manifestURL);
+      await act(instance, { manifestURL });
 
       // Assert
       expect(windowMoq.document.createElement).toHaveBeenCalledWith("script");
@@ -86,7 +101,7 @@ describe(AssetServices.name, () => {
       const instance = new AssetServices(windowMoq as any);
 
       // Act
-      await instance.load(manifestURL);
+      await act(instance, { manifestURL });
 
       // Assert
       expect(windowMoq.document.createElement).toHaveBeenCalledWith("link");
@@ -123,127 +138,7 @@ describe(AssetServices.name, () => {
       const instance = new AssetServices(windowMoq as any);
 
       // Act
-      await instance.load(manifestURL, sources);
-
-      // Assert
-      expect(windowMoq.document.body.appendChild).toHaveBeenCalledTimes(
-        expectedJsAppendUrls.length
-      );
-      expectedJsAppendUrls.forEach((url) => {
-        expect(windowMoq.document.body.appendChild).toHaveBeenCalledWith({
-          async: false,
-          src: url,
-        });
-      });
-      expect(windowMoq.document.head.appendChild).toHaveBeenCalledTimes(
-        expectedCssAppendUrls.length
-      );
-      expectedCssAppendUrls.forEach((url) => {
-        expect(windowMoq.document.head.appendChild).toHaveBeenCalledWith({
-          rel: "stylesheet",
-          async: false,
-          href: url,
-        });
-      });
-    });
-  });
-
-  describe("load (with object literal parameter)", () => {
-    it("should load manifest from specified URL", async () => {
-      // Arrange
-      const manifestURL =
-        "https://cdn.fromdoppler.com/test/asset-manifest-v1.json";
-      const responseJson = {
-        entrypoints: [],
-      };
-      const windowMoq = createWindowMoq(responseJson);
-      const instance = new AssetServices(windowMoq as any);
-
-      // Act
-      await instance.load({ manifestURL });
-
-      // Assert
-      expect(windowMoq.fetch).toHaveBeenCalledTimes(1);
-      expect(windowMoq.fetch).toHaveBeenCalledWith(manifestURL);
-    });
-
-    it("should update create elements for javascript files", async () => {
-      // Arrange
-      const manifestURL =
-        "https://cdn.fromdoppler.com/test/asset-manifest-v1.json";
-      const responseJson = {
-        entrypoints: [
-          "static/js/main.cf47d9fd.js",
-          "https://absolute/absolute.js",
-        ],
-      };
-      const expectedCreatedScriptElements = responseJson.entrypoints.length;
-      const windowMoq = createWindowMoq(responseJson);
-      const instance = new AssetServices(windowMoq as any);
-
-      // Act
-      await instance.load({ manifestURL });
-
-      // Assert
-      expect(windowMoq.document.createElement).toHaveBeenCalledWith("script");
-      expect(windowMoq.document.createElement).toHaveBeenCalledTimes(
-        expectedCreatedScriptElements
-      );
-    });
-
-    it("should update create elements for css files", async () => {
-      // Arrange
-      const manifestURL =
-        "https://cdn.fromdoppler.com/test/asset-manifest-v1.json";
-      const responseJson = {
-        entrypoints: [
-          "https://absolute/absolute.css",
-          "static/js/main.cf47d9fd.css",
-        ],
-      };
-      const expectedCreatedLinkElements = responseJson.entrypoints.length;
-      const windowMoq = createWindowMoq(responseJson);
-      const instance = new AssetServices(windowMoq as any);
-
-      // Act
-      await instance.load({ manifestURL });
-
-      // Assert
-      expect(windowMoq.document.createElement).toHaveBeenCalledWith("link");
-      expect(windowMoq.document.createElement).toHaveBeenCalledTimes(
-        expectedCreatedLinkElements
-      );
-    });
-
-    it("should append elements from manifest and sources to the DOM", async () => {
-      // Arrange
-      const manifestURL =
-        "https://cdn.fromdoppler.com/test/asset-manifest-v1.json";
-      const responseJson = {
-        entrypoints: [
-          "static/css/main.e6c13ad2.css",
-          "static/js/main.cf47d9fd.js",
-          "https://absolute/file3.css",
-        ],
-      };
-      const sources = [
-        "relativeSource.js",
-        "https://absolute/absoluteSource.css",
-      ];
-      const expectedJsAppendUrls = [
-        "https://cdn.fromdoppler.com/test/static/js/main.cf47d9fd.js",
-        "relativeSource.js", // It should continue as relative
-      ];
-      const expectedCssAppendUrls = [
-        "https://cdn.fromdoppler.com/test/static/css/main.e6c13ad2.css",
-        "https://absolute/file3.css",
-        "https://absolute/absoluteSource.css",
-      ];
-      const windowMoq = createWindowMoq(responseJson);
-      const instance = new AssetServices(windowMoq as any);
-
-      // Act
-      await instance.load({ manifestURL, sources });
+      await act(instance, { manifestURL, sources });
 
       // Assert
       expect(windowMoq.document.body.appendChild).toHaveBeenCalledTimes(
